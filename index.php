cbda66a788a4077d2fb60e9c96ff74d665e7ae01
twent <twentprod@gmail.com>
2022-10-08 18:26:26 +0300

    🛠️ Refactoring main index.php

--- index.php
+++ index.php
@@ -1,12 +1,7 @@
 <?php
 
-$config = include $_SERVER['DOCUMENT_ROOT'] . '/src/config/app.php';
-require_once $config['ROOT_DIR'] . "/src/config/db/db_connection.php";
-require $config['ROOT_DIR'] . "/src/flash_messages.php";
+declare(strict_types=1);
 
-// Session Start
-if (!isset($_SESSION)) {
-    session_start();
-}
+require_once __DIR__ . "/vendor/autoload.php";
 
-require_once $config['ROOT_DIR'].'/src/config/routes.php';
+require_once __DIR__ . "/src/bootstrap/app.php";
---
+++ src/bootstrap/app.php
@@ -0,0 +1,26 @@
+<?php
+
+/** Bootstrap Web App
+ *
+ */
+
+declare(strict_types=1);
+
+use Carbon\Carbon;
+
+require_once $_SERVER['DOCUMENT_ROOT'] . "/src/config/paths.php";
+
+define('CONFIG', require_once CONFIG_DIR . "/app.php");
+
+/* Timezone and locale settings applying */
+Carbon::setLocale(CONFIG['LOCALE']);
+date_default_timezone_set(CONFIG['TIME_ZONE']);
+
+/* Load base components */
+require_once DATABASE_DIR . "/db_connection.php";
+require_once APP_DIR . "/start_session.php";
+require_once APP_DIR . "/flash_messages.php";
+require_once APP_DIR . "/redirect.php";
+
+/* Load router */
+require_once ROUTES_DIR . "/web.php";
