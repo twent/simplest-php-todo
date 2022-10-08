ef271d461d8490e3842aa092e3cc172152be973f
twent <twentprod@gmail.com>
2022-10-08 18:48:10 +0300

    ➕ Add about page template

---
+++ src/resources/views/pages/about.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en" data-theme="light" >
+
+    <head>
+        <?php include_once VIEWS_DIR . '/partials/head_tags.php' ?>
+    </head>
+
+    <body>
+        <?php include_once VIEWS_DIR . '/partials/header.php'; ?>
+        <!-- main -->
+        <section class="text-gray-600 body-font">
+            About Page
+        </section>
+
+    <?php include_once VIEWS_DIR . '/partials/footer.php'; ?>
+
+    <script src="/public/js/app.js"></script>
+    </body>
+</html>
